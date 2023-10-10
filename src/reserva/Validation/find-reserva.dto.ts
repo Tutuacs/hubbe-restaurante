c468eaa8f04383d57c8a2431f157debcaac0df68
Arthur Azevedo Da Silva <107653383+Tutuacs@users.noreply.github.com>
2023-10-10 15:21:10 -0300

    validate @MinDate

--- src/reserva/Validation/find-reserva.dto.ts
+++ src/reserva/Validation/find-reserva.dto.ts
@@ -1,9 +1,13 @@
-import { IsDate, IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
+import { IsDate, IsNotEmpty, IsNumber, IsOptional, MinDate } from 'class-validator';
 
 export class FindReservaDto {
   @IsNotEmpty()
   @IsDate({
     message: 'O campo Data deve ser escrito: ano-mes-dia => YYYY-MM-DD',
+  })
+  @MinDate(new Date(), {
+    message:
+      'A data deve ser maior que a data atual, como não pedimos horario obrigatoriamente as reservas duram o dia todo.',
   })
   data: Date;
 
--- src/reserva/Validation/validate-reserva.dto.ts
+++ src/reserva/Validation/validate-reserva.dto.ts
@@ -4,12 +4,17 @@ import {
   IsNotEmpty,
   IsNumber,
   IsOptional,
+  MinDate,
 } from 'class-validator';
 
 export class ValidateReservaDto {
   @IsNotEmpty()
   @IsDate({
     message: 'O campo Data deve ser escrito: ano-mes-dia => YYYY-MM-DD',
+  })
+  @MinDate(new Date(), {
+    message:
+      'A data deve ser maior que a data atual, como não pedimos horario obrigatoriamente as reservas duram o dia todo.',
   })
   data: Date;
 
